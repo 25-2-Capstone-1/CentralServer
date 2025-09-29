67657f55006f0968c90bfa23280b2407e41140a8
davidjk522 <davidjk522@gmail.com>
2025-09-29 16:07:52 +0900

    change to non-db opne port

---
+++ src/main/java/Controller/HomeController.java
@@ -0,0 +1,12 @@
+package Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home_start_page"; // This will render the home.html template
+    }
+}
