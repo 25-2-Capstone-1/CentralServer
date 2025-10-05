ee73384a23e9b98b8d0639dc1dfd85574faf6e2f
davidjk522 <davidjk522@gmail.com>
2025-10-06 00:57:46 +0900

    <commit type>: 서울시 OpenAPI 인구 밀도 불러오기
    <body>
    삼각지 역을 기준으로 필용한 정보들을 불러오는 기초 기능 구현(json 파일이 그냥 홈페이지에 뜸, 아직 서버는 구현x)
    <footer>

--- src/main/java/com/CentralServer/demo/controller/HomeController.java
+++ src/main/java/com/CentralServer/demo/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.CentralServer.demo.Controller;
+package com.CentralServer.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
