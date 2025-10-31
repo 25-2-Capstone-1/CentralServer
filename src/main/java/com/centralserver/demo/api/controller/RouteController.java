00a69dfca1ef02915f474fe5b53130f49b41d7a5
proud_oooof <parkjanghan2256@gmail.com>
2025-10-31 22:48:05 +0900

    feat: 경로 추천 mock data로 제공 업데이트 (수정 중)

--- src/main/java/com/centralserver/demo/api/controller/RouteController.java
+++ src/main/java/com/centralserver/demo/api/controller/RouteController.java
@@ -22,16 +22,15 @@ public class RouteController {
 
     @PostMapping("/recommend")
     public List<RecommendRouteDTO> recommendRoutes(@RequestBody RouteRequestDTO request) throws IOException {
-        // 1️⃣ 요청값 출력 (디버깅용)
-        System.out.println("추천 요청: " + request);
 
-        // 2️⃣ JSON 파일 2개 읽기 (src/main/resources/data/ 경로)
-        RecommendRouteDTO route1 = loadRouteFromJson("data/routes/RecommendRoute/RecommendRoute1.json");
-        RecommendRouteDTO route2 = loadRouteFromJson("data/routes/RecommendRoute/RecommendRoute2.json");
-        RecommendRouteDTO route3 = loadRouteFromJson("data/routes/RecommendRoute/RecommendRoute3.json");
+        // 백엔드에서 경로 추천 로직 적용
 
+        // 현재는 미리 만들어 둔 json 파일 사용
+        RecommendRouteDTO route1 = loadRouteFromJson("data/RecommendRoute/RecommendRoute1.json");
+        RecommendRouteDTO route2 = loadRouteFromJson("data/RecommendRoute/RecommendRoute2.json");
+        RecommendRouteDTO route3 = loadRouteFromJson("data/RecommendRoute/RecommendRoute3.json");
 
-        // 3️⃣ 여러 개의 추천 경로 반환
+        // 추천 경로 반환
         List<RecommendRouteDTO> routes = new ArrayList<>();
         routes.add(route1);
         routes.add(route2);
@@ -44,6 +43,8 @@ public class RouteController {
         ClassPathResource resource = new ClassPathResource(path);
         return objectMapper.readValue(resource.getInputStream(), RecommendRouteDTO.class);
     }
+
+
 //      선택된 내용 db 저장 로직 제거
 //    @PostMapping("/result")
 //    public ResultRouteDTO getSelectedRoute(@RequestBody RouteRequestDTO request) throws IOException {
@@ -55,4 +56,6 @@ public class RouteController {
 //        result.setMessage("선택된 경로입니다.");
 //        return result;
 //    }
+
+
 }
