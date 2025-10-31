2f8c92da75b8b5cecacdd176e88033552451bc40
proud_oooof <parkjanghan2256@gmail.com>
2025-10-31 23:10:38 +0900

    feat: RouteService와 Controller 역할 분리

--- src/main/java/com/centralserver/demo/domain/route/service/RouteService.java
+++ src/main/java/com/centralserver/demo/domain/route/service/RouteService.java
@@ -1,4 +1,53 @@
 package com.centralserver.demo.domain.route.service;
 
+import com.centralserver.demo.domain.route.dto.RecommendRouteDTO;
+import com.centralserver.demo.domain.route.dto.RouteRequestDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class RouteService {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Transactional
+    public List<RecommendRouteDTO> recommendRoutes(@RequestBody RouteRequestDTO request) throws IOException {
+
+        List<RecommendRouteDTO> routes = new ArrayList<>();
+
+        // 현재는 미리 만들어 둔 json 파일 사용
+        RecommendRouteDTO route1 = loadRouteFromJson("data/RecommendRoute/RecommendRoute1.json");
+        RecommendRouteDTO route2 = loadRouteFromJson("data/RecommendRoute/RecommendRoute2.json");
+        RecommendRouteDTO route3 = loadRouteFromJson("data/RecommendRoute/RecommendRoute3.json");
+
+        routes.add(route1);
+        routes.add(route2);
+        routes.add(route3);
+
+        return routes;
+    }
+
+    private RecommendRouteDTO loadRouteFromJson(String path) throws IOException {
+        ClassPathResource resource = new ClassPathResource(path);
+        return objectMapper.readValue(resource.getInputStream(), RecommendRouteDTO.class);
+    }
+
+    //      선택된 내용 db 저장 로직 제거
+//    @PostMapping("/result")
+//    public ResultRouteDTO getSelectedRoute(@RequestBody RouteRequestDTO request) throws IOException {
+//        // 사용자가 선택한 경로를 기준으로 최종 경로를 응답하는 예시
+//        RecommendRouteDTO selectedRoute = loadRouteFromJson("data/RecommendRoute1.json");
+//
+//        ResultRouteDTO result = new ResultRouteDTO();
+//        result.setRoute(selectedRoute);
+//        result.setMessage("선택된 경로입니다.");
+//        return result;
+//    }
 }
