95160266032a6b731c7395bb50bc8d6a21e9e7d9
proud_oooof <parkjanghan2256@gmail.com>
2025-11-06 14:03:08 +0900

    feat: get 매핑으로 유저 정보 조회 업데이트

---
+++ src/main/java/com/centralserver/demo/domain/user/dto/UserEmailDTO.java
@@ -0,0 +1,13 @@
+package com.centralserver.demo.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEmailDTO {
+    private String userEmail;
+
+}
