17b9ca1dcfd790cf09329ec89ce27f160a311002
proud_oooof <parkjanghan2256@gmail.com>
2025-11-27 13:24:11 +0900

    feat: 경로별 로드뷰 인터페이스 수정

--- src/main/java/com/centralserver/demo/api/controller/RoadViewController.java
+++ src/main/java/com/centralserver/demo/api/controller/RoadViewController.java
@@ -1,5 +1,6 @@
 package com.centralserver.demo.api.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +9,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class RoadViewController {
 
+    @Value("${kakao.api.key}")
+    private String kakaoApiKey;
+
     @GetMapping(value = "/roadview", produces = "text/html; charset=UTF-8")
     public String roadview(@RequestParam double lat, @RequestParam double lng, Model model) {
         model.addAttribute("lat", lat);
         model.addAttribute("lng", lng);
+        model.addAttribute("kakaoApiKey", kakaoApiKey);
         return "roadview";
     }
 }
